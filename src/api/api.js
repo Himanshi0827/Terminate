1b9736ad9a3f8234cfdfd8db7c90aa0bd3e5255e
Himanshi0827 <157987622+Himanshi0827@users.noreply.github.com>
2026-03-10 19:52:57 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -106,3 +106,28 @@ export async function getAgreementById(id) {
   }
 }
 
+const LINE_ITEM_URL =
+  "https://preview-rls09.congacloud.com/api/data/v1/objects/Agreement";
+
+export const fetchAgreement = async () => {
+
+//const response= await line(LINE_ITEM_URL, null,null,null);
+  const token = await getAccessToken();
+
+   //console.log("himanshi "+token);
+  const response = await axios.get(LINE_ITEM_URL, {
+    headers: {
+       Accept: "*/*",
+      Authorization: `Bearer ${token}`,
+     // "user-id": "6cfff136-e62b-d435-133d-455fb809c836",
+      "Access-Control-Allow-Origin":"*",
+      "Access-Control-Allow-Methods":"'GET, POST, OPTIONS, PUT, PATCH, DELETE",
+      "Access-Control-Allow-Headers":"origin,X-Requested-With,content-type,accept",
+      "Access-Control-Allow-Credentials":"true",
+        "Content-Type":"application/json"
+    },
+  });
+
+  return response.data.items || [];
+};
+
